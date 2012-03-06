665c92ed3fc90246cc3e9c072e18986a55d04b34
EdGruberman <ed@rjump.com>
2012-03-05 21:08:31 -0700

    FIX: PlayerItemHeldEvent not being recognized as supported

--- src/edgruberman/bukkit/playeractivity/filters/PlayerItemHeldEvent.java
+++ src/edgruberman/bukkit/playeractivity/filters/PlayerItemHeldEvent.java
@@ -2,14 +2,13 @@ package edgruberman.bukkit.playeractivity.filters;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.player.PlayerItemHeldEvent;
 
 import edgruberman.bukkit.playeractivity.Interpreter;
 
-public class PlayerItemHeldEventListener extends Interpreter {
+public class PlayerItemHeldEvent extends Interpreter {
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onEvent(final PlayerItemHeldEvent event) {
+    public void onEvent(final org.bukkit.event.player.PlayerItemHeldEvent event) {
         this.tracker.record(event.getPlayer(), event);
     }
 
