98bd4a0f89bec73998428f91cf04620e907f31d8
EdGruberman <ed@rjump.com>
2012-03-23 13:16:08 -0700

    CHG: Players entering or leaving bed will automatically trigger /back

--- src/edgruberman/bukkit/playeractivity/Main.java
+++ src/edgruberman/bukkit/playeractivity/Main.java
@@ -25,7 +25,7 @@ public final class Main extends JavaPlugin {
     public static IdleKick idleKick = null;
     public static AwayBack awayBack = null;
 
-    private static final String MINIMUM_CONFIGURATION_VERSION = "1.3.0b1";
+    private static final String MINIMUM_CONFIGURATION_VERSION = "1.3.0b2";
     private ConfigurationFile configurationFile;
 
     @Override
