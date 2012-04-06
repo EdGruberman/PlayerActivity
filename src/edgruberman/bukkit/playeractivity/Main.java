2069e45a833df724fb22ea527b392375beacd3c7
EdGruberman <ed@rjump.com>
2012-04-06 00:31:37 -0700

    1.4.0 release CHG: Dependency for MessageManager v6.1.0

--- src/edgruberman/bukkit/playeractivity/Main.java
+++ src/edgruberman/bukkit/playeractivity/Main.java
@@ -27,7 +27,7 @@ public final class Main extends JavaPlugin {
     public static AwayBack awayBack = null;
     public static ListTag listTag = null;
 
-    private static final String MINIMUM_CONFIGURATION_VERSION = "1.4.0a23";
+    private static final String MINIMUM_CONFIGURATION_VERSION = "1.4.0";
     private ConfigurationFile configurationFile;
 
     private static Main that = null;
