96ada1ef1deb46c7a24327fd97f10f253eae4992
EdGruberman <ed@rjump.com>
2012-08-17 23:31:05 -0700

    CHG: Add "away" metadata to players

--- src/edgruberman/bukkit/playeractivity/consumers/AwayBack.java
+++ src/edgruberman/bukkit/playeractivity/consumers/AwayBack.java
@@ -10,6 +10,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.Plugin;
 
 import edgruberman.bukkit.playeractivity.PlayerActive;
@@ -19,6 +20,7 @@ import edgruberman.bukkit.playeractivity.messaging.ConfigurationCourier;
 
 public class AwayBack implements Observer, Listener {
 
+    public final Plugin plugin;
     public final boolean overrideIdle;
     public final StatusTracker back;
     public final Mentions mentions;
@@ -28,6 +30,7 @@ public class AwayBack implements Observer, Listener {
     private final Map<Player, AwayState> away = new HashMap<Player, AwayState>();
 
     public AwayBack(final Plugin plugin, final ConfigurationSection config, final ConfigurationCourier courier) {
+        this.plugin = plugin;
         this.courier = courier;
         this.overrideIdle = config.getBoolean("overrideIdle");
 
@@ -42,16 +45,25 @@ public class AwayBack implements Observer, Listener {
         this.back.register(this, PlayerActive.class);
 
         this.mentions = (config.getBoolean("mentions") ? new Mentions(plugin, this.courier, this) : null);
+
+        for (final Player player : Bukkit.getOnlinePlayers())
+            player.setMetadata("away", new FixedMetadataValue(this.plugin, false));
     }
 
     public void unload() {
         if (this.mentions != null) this.mentions.unload();
         this.back.clear();
         this.away.clear();
+
+        for (final Player player : Bukkit.getOnlinePlayers())
+            player.removeMetadata("away", this.plugin);
     }
 
     public boolean setAway(final Player player, final String reason) {
         final AwayState state = new AwayState(player, System.currentTimeMillis(), reason);
+
+        player.setMetadata("away", new FixedMetadataValue(this.plugin, true));
+
         final PlayerAway custom = new PlayerAway(state.player, state.since, state.reason);
         Bukkit.getServer().getPluginManager().callEvent(custom);
 
@@ -67,8 +79,11 @@ public class AwayBack implements Observer, Listener {
 
         this.away.remove(player);
 
+        player.setMetadata("away", new FixedMetadataValue(this.plugin, false));
+
         final PlayerBack custom = new PlayerBack(state.player, state.since, state.reason);
         Bukkit.getServer().getPluginManager().callEvent(custom);
+
         return true;
     }
 
