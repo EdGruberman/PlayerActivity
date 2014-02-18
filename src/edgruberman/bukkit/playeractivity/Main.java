9535bb92fe86fbd6ddf1297b57efa1fba535bcaf
EdGruberman <ed@rjump.com>
2014-02-17 22:23:26 -0700

    release 4.3.0

--- src/edgruberman/bukkit/playeractivity/Main.java
+++ src/edgruberman/bukkit/playeractivity/Main.java
@@ -34,7 +34,7 @@ public final class Main extends CustomPlugin {
     @Override
     public void onLoad() {
         this.putConfigMinimum("4.0.0");
-        this.putConfigMinimum("language.yml", "4.2.2");
+        this.putConfigMinimum("language.yml", "4.3.0");
     }
 
     @Override
